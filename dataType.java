380c0c160e663b305531b2f332109623e56ed861
Hemani99 <49550195+Hemani99@users.noreply.github.com>
2021-03-31 10:11:57 +0530

    Create dataType.java

---
+++ dataType.java
@@ -0,0 +1,11 @@
+public class dataType {
+
+	public static void main(String[] args) {
+		int a=10;
+		double b=5.89;
+		char c='h';
+		System.out.println("integer is:"+a+" double is:"+b+" character is:"+c);
+
+	}
+
+}
