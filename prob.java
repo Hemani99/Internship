359a9c2a8d1fd81863bb4aa6648b98074634d89c
Hemani99 <49550195+Hemani99@users.noreply.github.com>
2021-03-31 10:26:35 +0530

    Create prob.java

---
+++ prob.java
@@ -0,0 +1,12 @@
+public class prod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double a=8.2;
+		int b=6;
+		double m=a*b;
+		System.out.println("the product is "+m);
+
+	}
+
+}
