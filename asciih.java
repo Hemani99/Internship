5103e069baa475a1dc940181f6613ea4ab4fe617
Hemani99 <49550195+Hemani99@users.noreply.github.com>
2021-03-31 10:33:45 +0530

    Create asciih.java

---
+++ asciih.java
@@ -0,0 +1,11 @@
+public class asciih {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char a='h';
+		int b=a;
+		System.out.println("The ascii vale of "+a+" is "+b);
+
+	}
+
+}
