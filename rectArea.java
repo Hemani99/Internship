9539eb8ea965d1b0439d91b58da1960b9d017ec8
Hemani99 <49550195+Hemani99@users.noreply.github.com>
2021-03-31 10:17:44 +0530

    Create rectArea.java

---
+++ rectArea.java
@@ -0,0 +1,12 @@
+public class rectArea {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int l=3;
+		int b=2;
+		int m=l*b;
+		System.out.println("the area is "+m);
+
+	}
+
+}
